4dd8178ba5a3e73e20cb8a62834cf98eef610922
stannapav <stannapav@gmail.com>
2025-05-08 16:07:40 +0300

    Deleted cast and crew

--- src/main/java/org/example/movieapp/db/repositories/CastRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.movieapp.db.repositories;
-
-import org.example.movieapp.db.entities.Cast;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CastRepository extends JpaRepository<Cast, Integer> {
-}
--- src/main/java/org/example/movieapp/db/repositories/CrewRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.movieapp.db.repositories;
-
-import org.example.movieapp.db.entities.Crew;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CrewRepository extends JpaRepository<Crew, Integer> {
-}
