4b13e30166a80782108424573c868f7b7d89850c
stannapav <stannapav@gmail.com>
2025-05-10 16:13:42 +0300

    Handle exception

---
+++ src/main/java/org/example/movieapp/exeptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package org.example.movieapp.exeptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
